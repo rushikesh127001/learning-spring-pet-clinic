aac4bca5daf3c218ca65bddb3372a589e45a627b
rushikesh127001 <chasrushikesh@gmail.com>
2021-12-06 20:55:23 +0530

    initial commit

---
+++ src/main/java/learning/spring/petclinicproject/PetClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package learning.spring.petclinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learning/spring/petclinicproject/PetClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.petclinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
