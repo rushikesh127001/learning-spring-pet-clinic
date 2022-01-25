cdeec79649c639c5f7c3be465c8af0c95536e412
rushikesh127001 <chasrushikesh@gmail.com>
2022-01-26 00:29:35 +0530

    added PetTypeService and PetTypeServiceMap

---
+++ pet-clinic-data/src/main/java/learning/spring/petclinicproject/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package learning.spring.petclinicproject.services;
+
+import learning.spring.petclinicproject.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/learning/spring/petclinicproject/services/map/PetTypeServiceMap.java
@@ -0,0 +1,34 @@
+package learning.spring.petclinicproject.services.map;
+
+import learning.spring.petclinicproject.models.PetType;
+import learning.spring.petclinicproject.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType entity) {
+        super.delete(entity);
+
+    }
+
+    @Override
+    public PetType save(PetType entity) {
+        return super.save(entity.getId(),entity);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
